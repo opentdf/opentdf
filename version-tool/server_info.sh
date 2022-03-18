c14d76041f20daebd3185480afc976e003c2126c
Elizabeth Healy <ehealy@virtru.com>
2022-03-18 14:08:18 -0400

    getting images via kybectl

--- version-tool/server_info.sh
+++ version-tool/server_info.sh
@@ -2,7 +2,7 @@
 
 echo  "-------------------------------Server Information----------------------------"
 
-echo "-----HELM-----"
+echo "-----HELM DEPENDENCIES-----"
 
 if [[ -z "$1" ]]; then
     echo "No chart supplied"
@@ -14,10 +14,32 @@ PATH_TO_CHART_DIR="$(dirname "$1")"
 
 helm dependency list $PATH_TO_CHART_DIR
 
-echo "-----DOCKER IMAGES-----"
+echo ""
+
+echo "-----DOCKER IMAGES FROM HELM-----"
 
 helm template $PATH_TO_CHART_DIR \
     | perl -ne 'print "$1\n" if /image: (.+)/' \
     | tr -d '"' \
     | sort -u
 
+echo "-----DOCKER IMAGES FROM KUBECTL-----"
+images=( $(kubectl get pods --all-namespaces -o jsonpath="{.items[*].status.containerStatuses[*].image}" |\
+tr -s '[[:space:]]' '\n') )
+imageIDs=( $(kubectl get pods --all-namespaces -o jsonpath="{.items[*].status.containerStatuses[*].imageID}" |\
+tr -s '[[:space:]]' '\n') )
+
+# echo "Image\nImageID"
+for i in "${!images[@]}"; do
+    printf "Image: %s\nImageID: %s\n\n" "${images[i]}" "${imageIDs[i]}"
+done
+
+
+# # lists all the images running in kubernetes cluster
+# kubectl get pods --all-namespaces -o jsonpath="{.status.containerStatuses[*].image}" |\
+# tr -s '[[:space:]]' '\n'
+
+# kubectl get pods --all-namespaces -o jsonpath="{.status.containerStatuses[*].imageID}" |\
+# tr -s '[[:space:]]' '\n'
+
+# kubectl get pods --all-namespaces -o json | jq '.items[] .status.containerStatuses[] | { "image": .image, "imageID": .imageID }'
