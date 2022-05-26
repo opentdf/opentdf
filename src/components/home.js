40f17a893d1844cd6b7c4d696de987b6fd77092e
cassandrabailey293 <cassandra.bailey@virtru.com>
2022-05-26 10:10:43 -0600

    PLAT-1627; restyle the navbar

--- src/components/home.js
+++ src/components/home.js
@@ -12,39 +12,19 @@ function Home() {
         <h1>Protect Your Data. Build Your Future.</h1>
         <p>The OpenTDF project is an open set of tools and services that allows you to protect your data everywhere it goes.</p>
         <div className="header-detail">
-          <p>
-            Just getting started? Check out our Quickstart Guide:
-          </p>
           <Button
-            className="light-blue-theme"
             appearance="primary"
             href="https://github.com/opentdf/documentation/tree/main/quickstart"
           >
-            Quickstart
+            Get Started
           </Button>
-          <p>
-            Ready to Integrate? Download packages here:
-          </p>
           <Button
-            className="light-blue-theme"
-            appearance="primary"
-            href="https://github.com/orgs/opentdf/packages"
-          >
-            Download Packages
-          </Button>
-          <p>
-            Want to learn more? Access examples, documentation, and more on Github:
-          </p>
-          <Button
-            className="light-blue-theme"
             appearance="primary"
             href="https://github.com/orgs/opentdf/"
           >
-            OpenTDF
+            Learn More
           </Button>
         </div>
-        {/* <img src={Banner} className="OpenTDF-Banner" alt="OpenTDF" /> */}
-        {/* <h1>Protect Your Data. Control Your Keys. Build Your Future.</h1> */}
       </div>
     </div>
   );
--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,31 +1,46 @@
 import { Navbar, Nav } from "rsuite";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
-import logo from '../opentdf-nav-logo-transparent.png';
+import logo from '../opentdf-name-logo.png';
+import githubIcon from '../github-logo.png';
+import twitterIcon from "../twitter-logo.png";
 import "../App.css";
 
-const githubIcon = <FontAwesomeIcon icon={faGithub} size="xl" color="#004B87"/>
-const twitterIcon = <FontAwesomeIcon icon={faTwitter} size="xl" color="#004B87"/>
 
 function NavigationBar() {
   return (
-    <Navbar style={{ background: "#061e49", height: "10vh"}}>
-      <Nav style={{paddingTop: "2vh"}}>
+    <Navbar style={{ background: "#061e49", height: "9vh" }}>
+      <Nav style={{ paddingTop: "1vh" }}>
         <Nav.Item>
-        <img src={logo} className="opentdf-logo" alt="logo" />
+          <img src={logo} className="opentdf-logo" alt="logo" />
         </Nav.Item>
       </Nav>
-      <Nav pullRight style={{padding: "2vh", color: "#004B87", fontWeight: "bold", fontSize:"2vh"}}>
+      <Nav
+        pullRight
+        style={{
+          padding: "1vh",
+          color: "white",
+          fontWeight: "bold",
+          fontSize: "2vh",
+        }}
+      >
         <Nav.Item href="https://opentdf.stoplight.io/docs/opentdf-full-api-documentation/branches/main/pqb9cavidvql2-open-tdf">
-            API
+          API
         </Nav.Item>
-        <Nav.Item 
-            icon={githubIcon}
-            href="https://github.com/opentdf">
+        <Nav.Item>
+          <img
+            src={githubIcon}
+            className="opentdf-logo"
+            alt="github"
+            href="https://github.com/opentdf"
+          />
         </Nav.Item>
-        <Nav.Item 
-            icon={twitterIcon}
-            href="https://twitter.com/openTDF">
+        <Nav.Item>
+          <img
+            src={twitterIcon}
+            className="opentdf-logo"
+            alt="twitter"
+            href="https://twitter.com/openTDF"
+          />
         </Nav.Item>
       </Nav>
     </Navbar>
