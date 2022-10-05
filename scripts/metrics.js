a4a46e2bd7258cd610fd33d38be8fc0a83b65949
cassandrabailey293 <cassandra.bailey@virtru.com>
2022-10-05 09:19:21 -0600

    PLAT-2046; add async calls

--- scripts/metrics.js
+++ scripts/metrics.js
@@ -1,20 +1,20 @@
 module.exports = ({github, context}) => {
     
     //Research
-    const views = github.rest.repos.getViews({
+    const views = await github.rest.repos.getViews({
         owner: context.repo.owner,
         repo: context.repo.repo,
         })
 
     //Activation
-    const clones = github.rest.repos.getClones({
+    const clones = await github.rest.repos.getClones({
         owner: context.repo.owner,
         repo: context.repo.repo,
         })
-    const forks = github.rest.repos.listForks({
+    const forks = await github.rest.repos.listForks({
     owner: context.repo.owner,
     repo: context.repo.repo,
-    }).length
+    })
 
     // Retained Usage: Opening Issues, opening PRs, writing comments, posting/commenting on Discussions
     return {views: views, clones: clones, forks: forks}
