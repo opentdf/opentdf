8e287d4f83e3bdbd7ee100283b7899edaf2831fa
Elizabeth Healy <ehealy@virtru.com>
2023-01-20 01:24:32 -0500

    does getting traffic work

--- scripts/metrics.js
+++ scripts/metrics.js
@@ -1,25 +1,59 @@
 async function getMetrics(github, context) {   
-    //Research
+    // Views -- last 14 days
     const views = await github.rest.repos.getViews({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
+        owner: process.env.OWNER,
+        repo: process.env.REPO,
+        })
+    
+    // Clones -- last 14 days
+    const clones = await github.rest.repos.getClones({
+        owner: process.env.OWNER,
+        repo: process.env.REPO,
         })
 
-    //Activation
-    const clones = await github.rest.repos.getClones({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        })
-    const forks = await github.rest.repos.listForks({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
+    // // Forks -- all time
+    // const forks = await github.rest.repos.listForks({
+    //     owner: process.env.OWNER,
+    //     repo: process.env.REPO,
+    // })
+
+    const get_info = await github.rest.repos.get({
+        owner: process.env.OWNER,
+        repo: process.env.REPO,
     })
+
+    // Stars -- all time listStargazersForRepo: ["GET /repos/{owner}/{repo}/stargazers"]
+    // const stars = await github.rest.repos.listStargazersForRepo({
+    //     owner: process.env.OWNER,
+    //     repo: process.env.REPO,
+    // })
+
+    // Watchers listWatchersForRepo: ["GET /repos/{owner}/{repo}/subscribers"]
+
+
+    // Commits -- last 14 days getParticipationStats: ["GET /repos/{owner}/{repo}/stats/participation"] -- add togethor last two
+    const commits = await github.rest.repos.getParticipationStats({
+        owner: process.env.OWNER,
+        repo: process.env.REPO,
+        })
+    
+
+    // Referral Sources -- last 14 days getTopReferrers: ["GET /repos/{owner}/{repo}/traffic/popular/referrers"]
+    const referrals = await github.rest.repos.getTopReferrers({
+        owner: process.env.OWNER,
+        repo: process.env.REPO,
+        })
+    
     const data = {
         timestamp: new Date(),
-        event_type: "opentdf-github",
+        event_type: process.env.EVENT_TYPE,
         views: views.data.count, 
         clones: clones.data.count, 
-        forks: forks.data.length
+        forks: get_info.data.forks_count,
+        stars: get_info.data.stargazers_count,
+        watchers: get_info.data.subscribers_count,
+        commits: commits.data.all[commits.data.all.length-1] + commits.data.all[commits.data.all.length-2],
+        top_referral_sources: referrals.data
     }
     // Retained Usage: Opening Issues, opening PRs, writing comments, posting/commenting on Discussions
     return data
